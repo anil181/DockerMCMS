e517f0ad108f6fb026a12481f441d9532027f4fb
Anil <anil181@users.noreply.github.com>
2020-03-02 21:03:13 +0530

    Update TimeApplication.java

--- NoEureka/time/src/main/java/com/example/demo/TimeApplication.java
+++ NoEureka/time/src/main/java/com/example/demo/TimeApplication.java
@@ -1,7 +1,11 @@
 package com.example.demo;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.TimeZone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -22,10 +26,24 @@ class HelloController {
 	@RequestMapping("/time")
 	public String sayHi() {
 		
-		DateFormat indianFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
-	    indianFormat.setTimeZone(TimeZone.getTimeZone("Asia/Kolkata"));
-	    	    
-	    Date date = new Date();  	  
-		return indianFormat.format(date);
+	DateFormat indianFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+	indianFormat.setTimeZone(TimeZone.getTimeZone("Asia/Kolkata"));
+	Date date = new Date();  	  
+	    
+        InetAddress ip = null;
+        String hostname = null;
+        try {
+            ip = InetAddress.getLocalHost();
+            hostname = ip.getHostName();
+            System.out.println("Your current IP address : " + ip);
+            System.out.println("Your current Hostname : " + hostname);
+ 
+        } catch (UnknownHostException e) {
+ 
+            e.printStackTrace();
+        }
+        
+	return indianFormat.format(date) + " << IP >> " + ip + " << Hostname >> " + hostname;
+
 	}
 }
